99ac55c2c3640c9f16b249fc5d02af9c83f2fae9
Jung Gyu Yoon <yjg30737@gmail.com>
2022-10-06 09:38:26 +0900

    Update pdf.py

--- src/python/pdf.py
+++ src/python/pdf.py
@@ -4,12 +4,12 @@ import sys
 import io
 
 try:
-    # 아래 두 줄은 한글을 콘솔에 출력 시 깨지지 않도록 하기 위함 
+    # to support UTF-8 in vsc console
     sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
     sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
     config = pdfkit.configuration()
-    # sys.argv[1]은 pdf로 변환할 html 파일명
+    # sys.argv[1] is a filename which will be converted
     htmlPath = os.path.join('static', sys.argv[1])
     os.chdir('data')
     htmlFile = os.path.join(os.getcwd(), htmlPath)
@@ -17,5 +17,5 @@ try:
     pdfkit.from_file(htmlFile, pdfFile)
 except Exception as e:
     print(e)
-    # 오류 시 종료 코드
+    # exit code when error occured
     sys.exit(1)
